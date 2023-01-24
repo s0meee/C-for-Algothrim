c5b9017f5863eca2472e5a1d3b37619316ea0d00
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-24 13:16:15 +0000

    typedef :: type alias generation, using

--- .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
+++ .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
@@ -1,16 +1,13 @@
-#include <bits/stdc++.h>> 
-//모든 C++ 표준 라이브러리가 포함된 헤더파일
-//헤더파일 include 시키다.(프로그램에 포함 시키다)
+#include <bits/stdc++.h>> ///모든 표준 라이브러리가 포함된 헤더파일
+// include 지시문 활용, 프로그램에 포함 시킴
 using namespace std; 
-//std라는 네임 스페이스를 사용
-//네임 스페이스 : 변수명에 범위 걸어 놓는 것 
-string a;
-//문자열 변수 a 선언 
-//<타입> <변수이름>
+// std라는 네임스페이스(변수명 중복 방지를 위해 변수명에 범위를 걸어 놓는 것)를 사용
+typedef int i;
+// int라는 타입을 i라는 새로운 별칭으로 바꿔서 표현
+// typedef <타입> <별칭>
+//타입 이름 새롭게 별칭으로 정의, 이름 대신 별칭으로 사용 가능
 int main() {
-  cin >> a; //변수 a 입력 받음
-  cout << a << "\n"; //변수 a를 출력
-  return 0; //main 함수를 종료시키는 return 0 
-  //프로세스 정상적 마무리 의미 (process exit call success)
-  //cpp파일 하나당 하나의 main함수만 만들 수 있음 
+  i a = 1; 
+  cout << a << '\n'; //변수 a출력
+  return 0; //main함수 종료시키는 return 0 
 }

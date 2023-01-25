807908f78e01a55650925cc4755dc557c56c9d03
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-25 23:15:47 +0000

    define :: constant, macro definition

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
-#include <bits/stdc++.h>> ///모든 표준 라이브러리가 포함된 헤더파일
-// include 지시문 활용, 프로그램에 포함 시킴
-using namespace std; 
-// std라는 네임스페이스(변수명 중복 방지를 위해 변수명에 범위를 걸어 놓는 것)를 사용
-typedef int i;
-// int라는 타입을 i라는 새로운 별칭으로 바꿔서 표현
-// typedef <타입> <별칭>
-//타입 이름 새롭게 별칭으로 정의, 이름 대신 별칭으로 사용 가능
-int main() {
-  i a = 1; 
-  cout << a << '\n'; //변수 a출력
-  return 0; //main함수 종료시키는 return 0 
+#include <bits/stdc++.h>>
+//모든 표준 라이브러리가 포함된 헤더파일을 include를 통해 프로그램에 포함 시킨다.
+using namespace std;
+//std라는 네임스페이스를 사용한다.
+//네임스페이스 : 변수명의 중복을 막기위해 변수의 사용범위를 지정해주는 것 
+#define PI 3.14159
+// #define <이름> <값>
+//PI는 3.14159로 정의
+#define loop(x,n) for(int x=0; x<n; x++)
+//loop(x,n)은 for(int x=0; x<n; x++)로 정의 
+
+int main(){
+  cout << PI << '\n';
+  //PI를 입력 받음
+  int sum = 0;
+  loop(i,10){
+    sum += i;
+  }
+  //for(int x=0; x<10; x++)
+  //0~9까지 loop문을 돈다.
+  //sum +=1 은 sum = sum + i과 같다.
+  cout << sum << '\n';
+  //sum을 출력함
+  return 0;
+  //main함수 종료 
 }

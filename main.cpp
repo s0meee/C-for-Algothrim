291a2a4006169690dd02c56c094906f72fab89cb
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-26 10:53:25 +0000

    scanf:: double type > integer type

--- main.cpp
+++ main.cpp
@@ -1,31 +1,12 @@
 #include <bits/stdc++.h>
-//헤더파일 include시키다.
-//C++의 모든 표준라이브러리 포함된 헤더파일
-//프로그램에 포함시키다.
 using namespace std;
-//std 라는 namespace를 사용함
-//namespace는 변수명 중복 방지를 위해 변수명에 범위 걸어 놓는 것
-int a;
-//int라는 타입을 가진 a라는 변수
-double b;
-//double라는 타입을 가진 b라는 변수
-char c;
-//char라는 타입을 가진 c라는 변수
+int a, b;
+double c;
 int main(){
-  scanf("%d %lf %c", &a, &b, &c);
-  //매개변수 
-  //%d : int
-  //%lf : double 
-  //%c : char
-  printf("%d\n", a);
-  printf("%lf\n", b);
-  printf("%c\n", c);
-  return 0;
+scanf("%d.%d", &a, &b);
+printf("\n%d %d\n", a, b);
+  
+scanf("%lf", &c);
+printf("%lf\n", c);
+return 0;
 }
-//입력형식이 까다로울때 사용
-//입력 : 23330
-//      233.23123
-//      a
-//출력 : 23330
-//      233.231230
-//      a

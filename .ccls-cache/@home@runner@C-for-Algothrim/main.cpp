dc63bfc4aa8523f9e5eab71da7cb21b66e213495
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-26 02:40:40 +0000

    cin:: Enter up to opening characters

--- .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
+++ .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
@@ -1,26 +1,16 @@
-#include <bits/stdc++.h>>
-//모든 표준 라이브러리가 포함된 헤더파일을 include를 통해 프로그램에 포함 시킨다.
+#include <bits/stdc++.h>
+//C++의 모든 표준라이브러리가 포함된 헤더파일을 include 시킨다.(프로그램에 포함 시킨다)
 using namespace std;
-//std라는 네임스페이스를 사용한다.
-//네임스페이스 : 변수명의 중복을 막기위해 변수의 사용범위를 지정해주는 것 
-#define PI 3.14159
-// #define <이름> <값>
-//PI는 3.14159로 정의
-#define loop(x,n) for(int x=0; x<n; x++)
-//loop(x,n)은 for(int x=0; x<n; x++)로 정의 
-
+//std라는 namespace를 사용
+//네임스페이스는 변수명 중복 방지를 위해 변수명에 범위를 걸어 놓는 것
+string a; 
+//string이라는 타입을 가진 a라는 변수
 int main(){
-  cout << PI << '\n';
-  //PI를 입력 받음
-  int sum = 0;
-  loop(i,10){
-    sum += i;
-  }
-  //for(int x=0; x<10; x++)
-  //0~9까지 loop문을 돈다.
-  //sum +=1 은 sum = sum + 1과 같다.
-  cout << sum << '\n';
-  //sum을 출력함
-  return 0;
-  //main함수 종료 
+  cin >> a; 
+  //변수 a를 입력 받음
+  cout << a << "\n"; 
+  //변수 a를 출력함
+  return 0; 
+  //main함수 종료
+  //프로세스 정상적 마무리
 }

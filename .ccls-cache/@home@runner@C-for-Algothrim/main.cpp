3b40ebbe4abda43d12e365ab9c08831413642919
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-26 23:25:56 +0000

    getline :: received all at once

--- .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
+++ .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
@@ -1,12 +1,20 @@
 #include <bits/stdc++.h>
+//헤더파일을 include 시킴
+//C++의 모든 표준 라이브러리를 포함하는 헤더파일
+//프로그램에 포함시킨다.
 using namespace std;
-int a, b;
-double c;
-int main(){
-scanf("%d.%d", &a, &b);
-printf("\n%d %d\n", a, b);
-  
-scanf("%lf", &c);
-printf("%lf\n", c);
-return 0;
+//std라는 이름을 가진 네임스페이스를 사용
+//네임스페이스 : 변수명 중복을 막기 위해 변수명에 범위를 걸어 놓는 것
+string s;
+//문자열 변수 선언 <타입><변수명>
+//string이라는 타입을 가진 a라는 변수
+int main() {
+  getline(cin,s);
+  //변수 s 입력
+  cout << s << '\n';
+  //변수 s출력
+  return 0; 
+  //main함수 종료
+  //프로세스 정상적 마무리
 }
+

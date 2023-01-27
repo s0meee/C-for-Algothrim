b889bceee88e879f44f01988a33fb6624d9be233
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-27 00:22:34 +0000

    cout :: float, double type output

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-string a = "코딩테스트 화이팅";
-string b = "안녕하세요 저는 00 입니다";
+typedef long long ll;
+double a = 1.23456789;
 int main(){
   cout << a << "\n";
-  cout << a << " " << " " << b << "\n";
-  return 0;
+  cout.precision(7);
+  cout << a << '\n';
 }
 

fb9b84f46a6b64ec31906923b7bf8e9ac4a62451
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-28 11:22:27 +0000

    Function that returns Char

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+char b() {
   char a = 'a';
-  cout << a << "\n";
+  return a;
+}
+int main() {
+  char a = b();
+  cout << a << '\n';
   return 0;
 }
-//문자 a선언
-//a출력

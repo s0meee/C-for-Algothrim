8e3f34a516d1de7496ad624b57a10c886badad3a
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-28 07:09:39 +0000

    printf : formatting output

--- .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
+++ .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
+//C++의 모든 표준라이브러리가 포함된 헤더파일을 포함 시킴
+//프로그램에 포함
 using namespace std;
-typedef long long ll;
-double a = 1.23456789;
+//std라는 namespace를 사용
+//네임스페이스 - 변수명 중복 방지를 위해 변수명에 범위를 걸어 놓은 것
+int a = 1, b = 2;
+// int타입을 가진 a 라는 변수는 1이고,
+// int 타입을 갖
 int main(){
-  cout << a << "\n";
-  cout.precision(7);
-  cout << a << '\n';
+  printf("KOREA %d : Australia %d\n",a,b);
+  return 0;
+  //main함수 종료
 }
 

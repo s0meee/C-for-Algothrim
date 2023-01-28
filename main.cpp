7d2861e9c83454db7bcac36709a00a3e129d3db3
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-28 10:18:02 +0000

    scanf :: Format output

--- main.cpp
+++ main.cpp
@@ -1,25 +1,28 @@
 #include <bits/stdc++.h>
-//C++의 모든 표준라이브러리가 포함된 헤더파일을 포함 시킴
+// C++의 모든 표준라이브러리가 포함된 헤더파일을 포함 시킴
 //프로그램에 포함
 using namespace std;
-//std라는 namespace를 사용
+// std라는 namespace를 사용
 //네임스페이스 - 변수명 중복 방지를 위해 변수명에 범위를 걸어 놓은 것
-typedef long long ll;
-//long long 이라는 타입을 ll으로 별칭
-double a = 1.23456789;
-//double이라는 타입을 가진 a라는 변수에 1.23456789값을 할당
-int b = 2;
-//int라는 타입을 가진 b라는 변수에 2할당
-int main(){
-  printf("%.6lf\n",a);
-  // double 형 출력
-  //소수점 6자리까지 출력
-  printf("%02d\n",b);
-  //원하는 자리수만큼 비면 0을 앞에 붙여서 출력하기
-  //2자릿수 만큼 출력, 만약 a이 한자리수 일경우 앞에 0을 붙여서 출력함
+int a = 1;
+//int라는 타입을 가진 a라는 변수에 1 할당
+char s = 'a';
+//char이라는 타입을 가진 s라는 변수에 a 할당
+string str = "어벤져스";
+//string 이라는 타입을 가진 str라는변수에 어벤져스 할당
+double b = 1.223123;
+//double 이라는 타입을 가진 b라는 변수에 1.223123 할당
+
+int main() {
+  printf("아이엠 아이언맨 : %d\n", a);
+  //%d, int 타입
+  printf("아이엠 아이언맨 : %c\n", s);
+  //%c, char 타입
+  printf("아이엠 아이언맨 : %s\n", str.c_str());
+  //%s, string 타입
+  printf("아이엠 아이언맨 : %lf\n", b);
+  //%lf, double 타입
   return 0;
-  //main함수 종료
   //프로세스 정상적 마무리
+  //main 함수 종료
 }
-//1.234568
-//02

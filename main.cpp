7453ca272125f6804cf4e62cdce67d81e8869db5
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-28 10:35:13 +0000

    (Preferred not to write) Set declaration and definition separation functions

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+int main(){
+  double ret = a();
+  cout << ret << "\n";
+  return 0;
+}
+
 double a(){
   return 1.2333;
 }
-//a라는 함수 double 타입 정의
-int main(){
-  double ret = a();
-  cout << ret << '\n';
-  return 0;
-}
-//1.2333이라는 double 타입변수 return
-//선언, 정의 한꺼번에

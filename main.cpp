24f6f66ccce210f9e9dd77baae0deba876efbb68
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-28 10:29:11 +0000

    void : function that returns nothing

--- main.cpp
+++ main.cpp
@@ -1,22 +1,12 @@
 #include <bits/stdc++.h>
-//C++의 모든 표준라이브러리가 포함된 헤더파일을 include 시킨다.(프로그램에 포함 시킨다)
 using namespace std;
-//std라는 namespace를 사용
-//네임스페이스는 변수명 중복 방지를 위해 변수명에 범위를 걸어 놓는 것
-string a,b; 
-//string이라는 타입을 가진 a라는 변수
-int main(){
-  cin >> a >> b; 
-  //변수 a,b 입력받음
-  cout << a << "\n"; 
-  //변수 a를 출력함
-  cout << b << "\n"; 
-  //변수 b를 출력함
-  return 0; 
-  //main함수 종료
-  //프로세스 정상적 마무리
+int ret = 1;
+void a() {
+  ret = 2;
+  cout << ret << "\n";
+  return;
 }
-//개행문자를 넣어 2개의 문자열을 입력
-//입력: hello world
-//출력: hello
-//      word
+int main() {
+  a();
+  return 0;
+}

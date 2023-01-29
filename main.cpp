9321fffb4491db167334a3d53c1e6e10d7a1d70e
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-29 14:43:04 +0000

    String: One letter, 3 bytes in Korean, 1 byte in English

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-char b() {
-  char a = 'a';
-  return a;
-}
-int main() {
-  char a = b();
-  cout << a << '\n';
-  return 0;
+int main(){
+string a = "나는야";
+cout << a[0] << "\n"; //?
+cout << a[0] << a[1] << a[2] << '\n'; //나
+cout << a << "\n"; //나는야
+string b = "abc"; 
+cout << b[0] << "\n";  //a
+cout << b << "\n"; //abc
+return 0;
 }

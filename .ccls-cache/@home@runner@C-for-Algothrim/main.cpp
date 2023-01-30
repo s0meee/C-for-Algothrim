48dae0683bba500dfca9e8d75cffdd8c35972434
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-30 13:52:12 +0000

    ASCII  Code:: 97 = a, 65 = A

--- .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
+++ .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
@@ -4,16 +4,23 @@
 using namespace std;
 //std라는 네임스페이스 사용
 // 네임스페이스 - 변수명의 중복을 방지하기위해 변수명의 범위를 정함
-typedef long long ll;
-//타입 별칭 지정
 int main() {
-  ios::sync_with_stdio(false);
+  ios_base :: sync_with_stdio(false);
+  // c의 stdio와 cpp의 iostream을 동기화시켜주는 역할
+  // 동기화 > 비활성화
   cin.tie(NULL);
-  string s = "123"; 
-  s[0]++;
-  //코드 123에서 s[0]에 1을 더해 223
-  //아스키코드 49에 1더한 값 50
-  cout << s << "\n";
-  //
+  //입력
+  cout.tie(NULL);
+  //출력
+  //입출력 속도 빨라짐
+  //동기화를 끊음으로서 C++ stream들은 독립적인 buffer를 갖게됨
+  //buffer의 수가 줄어들며 속도가 빨라짐
+  char a = 'a';
+  //char라는 타입을 가진 a라는 변수에 a할당
+  cout << (int)a << '\n';
+  //(int)a를 통해 문자열 char을 정수 int로 변환
+  //문자 a는 97로 반환
   return 0;
+  //main 함수 종료
+  //프로세스 정상적 마무리
 }

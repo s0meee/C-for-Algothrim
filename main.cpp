b1743e4de57cf5faeade6a014cc0855d077c9015
s0meee <121744632+s0meee@users.noreply.github.com>
2023-01-30 14:09:39 +0000

    reverse(): to reverse

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
 #include <bits/stdc++.h>
-//C++의 모든 표준 라이브러리가 포함된 헤더파일
-//프로그램에 포함시킨다.
 using namespace std;
-//std라는 네임스페이스 사용
-// 네임스페이스 - 변수명의 중복을 방지하기위해 변수명의 범위를 정함
-int main() {
-  ios_base :: sync_with_stdio(false);
-  // c의 stdio와 cpp의 iostream을 동기화시켜주는 역할
-  // 동기화 > 비활성화
-  cin.tie(NULL);
-  //입력
-  cout.tie(NULL);
-  //출력
-  //입출력 속도 빨라짐
-  //동기화를 끊음으로서 C++ stream들은 독립적인 buffer를 갖게됨
-  //buffer의 수가 줄어들며 속도가 빨라짐
-  char a = 'a';
-  //char라는 타입을 가진 a라는 변수에 a할당
-  cout << (int)a << '\n';
-  //(int)a를 통해 문자열 char을 정수 int로 변환
-  //문자 a는 97로 반환
-  return 0;
-  //main 함수 종료
-  //프로세스 정상적 마무리
+int main(){
+string a = "It's hard to have a sore leg";
+reverse(a.begin(), a.end());
+cout << a << '\n';
+  //reverse(문자열 첫번 째, 문자열 마지막 째)
+  //gel eros a evah ot drah s'tI
+reverse(a.begin(), a.end());
+cout << a << '\n';
+  //It's hard to have a sore leg
+reverse(a.begin() + 3, a.end());
+cout << a << '\n';
+  //It's hard to have a sore leg
+  //0123 
+  //3번째 s부터 뒤집기 시작
+  //It'gel eros a evah ot drah s
+return 0;
 }

15f9c15fa56c294674a15b855e718a25d914f40d
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-01 14:11:20 +0000

    Range-based for loop example code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
-for ( range_declaration : range_expression )
-loop_statement
-//범위기반 for 루프
-for(string b : a) cout << b << "\n";
-//vector a내에 있는 요소 string타입의 요소 탐색
-for(int i = 0; i < a.size(); i++) cout << a[i] << "\n"
-  //위의 코드와 같은 의미
+#include<bits/stdc++.h>
+//c++의 모든 표준라이브러리를 포함하는 헤더파일
+//헤더파일을 include시킴
+//프로그램에 포함 시킴
+using namespace std;
+//std라는 네임스페이스를 사용
+//네임스페이스 : 변수명의 중복을 방지하기 위해 변수명의 범위 걸어 놓는 것
+string a[2] = {"out of time", "i love you"};
+int main(){
+for(string b : a) cout << b << '\n';
+  //vector a내의 요소인 string 타입의 요소를 탐색한다
+for(int i = 0; i < 2; i++) cout << a[i] << "\n";
+}

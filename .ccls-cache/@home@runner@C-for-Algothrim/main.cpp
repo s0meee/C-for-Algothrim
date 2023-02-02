598ec04b1cd8b339540fc5818b7239bb3e1e7eab
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-02 14:54:04 +0000

    int, an integer of 4 bytes

--- .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
+++ .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
@@ -1,14 +1,1 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-ios_base::sync_with_stdio(false);
-cin.tie(NULL); cout.tie(NULL);
-int a = -1;
-cout << bool(a) << "\n";
-a = 0;
-cout << bool(a) << "\n";
-a = 3;
-cout << bool(a) << "\n";
-}
-//C++은 false, 0
-//0이 아닌 것은 모두 1(true)
+//int, 4바이트짜리 정수

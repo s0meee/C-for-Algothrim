417e3b00c7a64bb504273827af2872cce4f33f88
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-03 14:44:13 +0000

    tie : Function used to extract values  pair, tuple

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
-typedef long long ll;
-ll INF = 1e18; 
-//long long, 8바이트 정수 
-double pi = 3.221;
-//double 실수 타입
-const int INF = 987654321;
-const int INF2 = 1e9;
-//int, 4바이트 정수
+#include<bits/stdc++.h>
+using namespace std;
+pair<int, int> pi;
+tuple<int, int, int> tl;
+int a, b, c;
+int main(){
+pi = {1, 2};
+tl = make_tuple(1, 2, 3);
+tie(a, b) = pi;
+cout << a << " : " << b << "\n";
+tie(a, b, c) = tl;
+cout << a << " : " << b << " : "<< c << "\n";
+return 0;
+}

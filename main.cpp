36d3791c681c72d39ca58286fcfdb317b72e10e5
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-03 15:11:51 +0000

    Auto Type : A simple way to replace complex and long type variable names

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int a = 1;
-auto b = 1;
 int main(){
-cout << b << '\n';
+vector<pair<int, int>> v;
+for(int i = 1; i <= 5; i++){
+v.push_back({i, i});
+}
+for(auto it : v){
+cout << it.first << " : " << it.second << '\n';
+}
+for(pair<int, int> it : v){
+cout << it.first << " : " << it.second << '\n';
+}
+return 0;
 }

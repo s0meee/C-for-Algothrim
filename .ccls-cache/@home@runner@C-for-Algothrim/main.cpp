d915b5a0bdbb3319426a5f6fce36e3af5ce922d0
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-03 15:10:06 +0000

    Auto type: Type determined by reasoning

--- .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
+++ .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
@@ -1,17 +1,7 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-pair<int, int> pi;
-tuple<int, int, int> ti;
-int a, b, c;
+int a = 1;
+auto b = 1;
 int main(){
-pi = {1, 2};
-a = pi.first;
-b = pi.second;
-cout << a << " : " << b << "\n";
-ti = make_tuple(1, 2, 3);
-a = get<0>(ti);
-b = get<1>(ti);
-c = get<2>(ti);
-cout << a << " : " << b << " : "<< c << "\n";
-return 0;
+cout << b << '\n';
 }

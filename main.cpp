534a1fc007b2e3f616edb7ac92bf7100589960ef
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-04 14:46:13 +0000

    Type conversion

--- main.cpp
+++ main.cpp
@@ -1,15 +1,12 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main(){
-vector<pair<int, int>> v;
-for(int i = 1; i <= 5; i++){
-v.push_back({i, i});
+  double ret = 2.12345;
+  int n = 2;
+  int a = (int)round(ret/double(n));
+  cout << a << '\n';
+  return 0;
 }
-for(auto it : v){
-cout << it.first << " : " << it.second << '\n';
-}
-for(pair<int, int> it : v){
-cout << it.first << " : " << it.second << '\n';
-}
-return 0;
-}
+//ret이란 double 타입에 int인 n을 연산해주기 위해서 
+//(바꿀타입) 기존 변수 
+//형 변환 계산을 진행 

816d6bfe0ca146df398aa6292334afe927df9bc1
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-04 14:47:59 +0000

    Precautions for Type Conversion

--- .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
+++ .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
@@ -1,12 +1,7 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main(){
-  double ret = 2.12345;
-  int n = 2;
-  int a = (int)round(ret/double(n));
-  cout << a << '\n';
-  return 0;
-}
-//ret이란 double 타입에 int인 n을 연산해주기 위해서 
-//(바꿀타입) 기존 변수 
-//형 변환 계산을 진행 
+int a = (int)p * 100
+//타입변환 일어남
+int a = (int)100 * p
+//타입변환 일어나지 않음 
+
+//타입변환 
+//(변환할 타입 ) 기존 변수임을 유의하자

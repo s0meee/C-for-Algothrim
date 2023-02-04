4a5e0cf51c8ccfb2149df07e93a3a355c734b6ca
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-04 15:04:21 +0000

    Pointer : type to hold the address, type to point to the memory address of the variable

--- .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
+++ .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int i;
+string s = "Somi";
 int main(){
-  cout << &i << '\n';
   i = 0;
-  cout << &i << '\n';
+  int * a = &i;
+  cout << a << '\n';
+  string * b = &s;
+  cout << b << '\n';
   return 0;
 }
-// i에 0 할당
-//예약한 메모리 영역에 해당 값 저장
-//메모리에 어떤값을 넣어도 주소는 변하지 않음 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int i;
+string s = "Somi";
 int main(){
-  cout << &i << '\n';
   i = 0;
-  cout << &i << '\n';
+  int * a = &i;
+  cout << a << '\n';
+  string * b = &s;
+  cout << b << '\n';
   return 0;
 }
-// i에 0 할당
-//예약한 메모리 영역에 해당 값 저장
-//메모리에 어떤값을 넣어도 주소는 변하지 않음 
+//포인터의 크기 고정되지 X
+//1바이트 짜리의 char타입 변수라고 해서 포인터의 크기가 1은 아님 

98948dd55e4bd2156a082a0be49e4d4ad09ffb68
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-04 15:15:53 +0000

    &a[1] expressed as a+1

--- .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
+++ .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
@@ -1,10 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[3] = {1,2,3};
-int main(){
+int a[3] = {1, 2, 3};
+int main() {
   int * b = &a[0];
   int * c = a;
+  int * d = (a+1);
   cout << b << '\n';
   cout << c << '\n';
-  return 0
+  cout << d << '\n';
+  cout << &a[1] << '\n';
+  return 0;
 }
+//&a[1] 을 a+1로 표현
--- main.cpp
+++ main.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 int a[3] = {1, 2, 3};
 int main() {
-  int *b = &a[0];
-  int *c = a;
+  int * b = &a[0];
+  int * c = a;
+  int * d = (a+1);
   cout << b << '\n';
   cout << c << '\n';
+  cout << d << '\n';
+  cout << &a[1] << '\n';
   return 0;
 }
-//&a[0] = a 같은 의미 
+//&a[1] 을 a+1로 표현

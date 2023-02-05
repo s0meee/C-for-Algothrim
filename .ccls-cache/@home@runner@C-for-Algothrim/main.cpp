663b8ca3b18f72c3978fb724e826faebc7596390
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-05 08:32:45 +0000

    Iterator : Object pointing to the address of an element stored in a container, pointer generalization

--- .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
+++ .ccls-cache/@home@runner@C-for-Algothrim/main.cpp
@@ -1,8 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int a = 1;
-const int b = 2;
-int main(){
-static int c = 3;
-return 0;
-} //Datasegment
+vector<int> v;
+int main() {
+  for (int i = 1; i <= 5; i++)
+    v.push_back(i);
+  for (int i = 0; i < 5; i++) {
+    cout << i << "번째 요소 : " << *(v.begin() + i) << "\n";
+    cout << &*(v.begin() + i) << '\n';
+  }
+  for (auto it = v.begin(); it != v.end(); it++) {
+    cout << *it << ' ';
+  }
+  cout << '\n';
+  for (vector<int>::iterator it = v.begin(); it != v.end(); it++) {
+    cout << *it << ' ';
+  }
+  auto it = v.begin();
+  advance(it, 3);
+  cout << '\n';
+  cout << *it << ' ';
+  // cout << v.begin() << '\n'; 에러
+}
+//auto - vector<int>::iterator 같이 사용
+//이터레이터  : 컨테이너에 저장된 요소의 주소 가리키는 객체, 포인터 일반화

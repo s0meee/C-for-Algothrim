c10164531a4637def89da97ba9710995137c2dd8
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-05 09:19:52 +0000

    memcpy(): Used to copy values in memory of one variable to "specific memory values" of another variable

--- main.cpp
+++ main.cpp
@@ -1,13 +1,1 @@
-#include <bits/stdc++.h>
-using namespace std;
-const int max_n = 1004;
-int a[max_n];
-int a2[max_n][max_n];
-int main() {
-memset(a, -1, sizeof(a));
-memset(a2, 0, sizeof(a2));
-for(int i = 0; i < 10; i++) cout << a[i] << " ";
-return 0;
-}
-//간편하게 초기화 
-//0,-1로 초기화 될때 memset쓰는 것이 좋음
+void * memcpy ( void * destination, const void * source, size_t num );

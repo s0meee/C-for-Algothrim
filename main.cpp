888a817c7cb4af79029768c72078c17846b26157
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-05 09:18:44 +0000

    memset() : Initialize in bytes and initialize with one character (a, b, c, etc.) of 0, -1, char type.

--- main.cpp
+++ main.cpp
@@ -1,2 +1,13 @@
-void * memset ( void * ptr, int value, size_t num );
-//memset(배열의 이름, k, 배열의 크기) 
+#include <bits/stdc++.h>
+using namespace std;
+const int max_n = 1004;
+int a[max_n];
+int a2[max_n][max_n];
+int main() {
+memset(a, -1, sizeof(a));
+memset(a2, 0, sizeof(a2));
+for(int i = 0; i < 10; i++) cout << a[i] << " ";
+return 0;
+}
+//간편하게 초기화 
+//0,-1로 초기화 될때 memset쓰는 것이 좋음

fe38d22f7376119570f4741e62e5d9c004116467
s0meee <121744632+s0meee@users.noreply.github.com>
2023-02-05 09:27:10 +0000

    memcpy(): Used to copy values in memory of one variable to "specific memory values" of another variable

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
-void * memcpy ( void * destination, const void * source, size_t num );
+#include <bits/stdc++.h>
+using namespace std;
+int a[5], temp[5];
+int main(){
+  for(int i = 0; i < 5; i++)a[i] = i;
+  memcpy(temp, a, sizeof(a));
+  for(int i : temp) cout << i << ' ';
+  cout << '\n';
+  // 원본 배열 a를 수정하여 출력하는 로직
+  // a를 수정해서 더하는 로직
+  a[4] = 1000;
+  for(int i : a) cout << i << ' ';
+  cout << '\n';
+  // 그 다음 다시 temp를 기반으로 원본배열을 담아 둠.
+  memcpy(a, temp, sizeof(temp));
+  for(int i : a) cout << i << ' ';
+  cout << '\n';
+  return 0;
+}
